49a7297a461ce1751216ae12256ba7132aec89a1
Jethalal Gada <ztejas@amazon.com>
2024-08-14 19:07:42 +0530

    Fetch data from mysql server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import AddTodo from "./components/jsxFiles/AddTodo"
 import AppName from "./components/jsxFiles/AppName"
 import Component from "./components/jsxFiles/Component"
@@ -13,13 +13,57 @@ function App() {
   const addNewItem = () => {
     const newItem = [...todoItems, {name: refName.current.value, dueDate: refDate.current.value}]
     setTodoItems(newItem)
+    const postData = {
+      name: refName.current.value,
+      dueDate: refDate.current.value
+    }
     refName.current.value = "";
     refDate.current.value = "";
+
+    fetch('http://localhost:3000/api/items', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
   };
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/items')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data)
+      setTodoItems(data)
+    })
+  }, []);
 
   const deleteItem = (itemName) => {
     const newTodoItem = todoItems.filter((item) => item.name !== itemName);
     setTodoItems(newTodoItem)
+    const encodedName = encodeURIComponent(itemName);
+    const url = `http://localhost:3000/api/items?name=${encodedName}`;
+    fetch(url, {
+      method: 'DELETE',
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Item deleted successfully');
+        setTodoItems(prevItems => prevItems.filter(item => item.name !== name));
+      } else {
+        console.error('Failed to delete item');
+      }
+    })
+    .catch(error => console.error('Error:', error));
   };
 
   return <>

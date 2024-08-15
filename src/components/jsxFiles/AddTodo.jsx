3be5fa4976cccaffeb824301fdfda55ee27e1675
jeel8599 <75315671+jeel8599@users.noreply.github.com>
2024-08-15 23:15:45 +0530

    Add files via upload

--- src/components/jsxFiles/AddTodo.jsx
+++ src/components/jsxFiles/AddTodo.jsx
@@ -2,14 +2,21 @@ import { useContext } from 'react'
 import styles from '../cssFiles/AddTodoInputs.module.css'
 import { TodoItemsContext } from '../../store/todo-items-store'
 
-function AddTodo({refName, refDate}) {
+function AddTodo({refName, refDate, refPriority}) {
   const contextObj = useContext(TodoItemsContext);
   const addNewItem = contextObj.addNewItem;
 
   return <div class="row">
-                  <div class="col-4"><input type="text" ref={refName} placeholder="Enter Todo here" className={`${styles['cool-input-box']}`}/></div>
-                  <div class="col-4"><input type="date" ref={refDate} className={`${styles['cool-input-box']}`}/></div>
-                  <div class="col-2"><button type="button" class={`btn btn-success ${styles['custom-button']}`} onClick={addNewItem}>Add</button></div>
+                  <div class="col-3"><input type="text" ref={refName} placeholder="Enter Todo here" className={`${styles['cool-input-box']}`}/></div>
+                  <div class="col-3"><input type="date" ref={refDate} className={`${styles['cool-input-box']}`}/></div>
+                  <div className="col-3">
+                    <select ref={refPriority} className={`${styles['cool-input-box']}`}>
+                      <option value="low">Low</option>
+                      <option value="medium">Medium</option>
+                      <option value="high">High</option>
+                    </select>
+                  </div>
+                  <div class="col-3"><button type="button" class={`btn btn-success ${styles['custom-button']}`} onClick={addNewItem}>Add</button></div>
           </div>
 }
 
